284bb710340fb1e12a94bb06ed0d473dbc8ae295
brianpino11 <brianpino015@gmail.com>
2025-11-19 09:14:24 -0500

    Se realiza el Ejercicio 2: Lista de Nombres (map con objetos)

--- BrahianEstibenPinoPrado/script.js
+++ BrahianEstibenPinoPrado/script.js
@@ -4,3 +4,13 @@ let puntosDoble = puntos.map(num => num * 2);
 
 console.log(puntos);
 console.log(puntosDoble);  
+
+//Ejercicio 2: Lista de Nombres (map con objetos)
+let usuarios = [
+  { id: 1, nombre: "Ana" },
+  { id: 2, nombre: "Luis" },
+  { id: 3, nombre: "Carlos" }
+];
+
+let nombres = usuarios.map(u => u.nombre);
+console.log(nombres);
