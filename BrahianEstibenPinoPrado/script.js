69774af6821fc55957ac685f6bc5e933880b96e4
brianpino11 <brianpino015@gmail.com>
2025-11-19 09:29:43 -0500

    Se realiza el Ejercicio 14: Invertir sin Mutar (Copia + reverse)

--- BrahianEstibenPinoPrado/script.js
+++ BrahianEstibenPinoPrado/script.js
@@ -97,3 +97,11 @@ let invitadosOrdenados = [...invitados].sort();
 
 console.log(invitados);       
 console.log(invitadosOrdenados); 
+
+//Ejercicio 14: Invertir sin Mutar (Copia + reverse)
+let receta = ["Paso 1", "Paso 2", "Paso 3"];
+
+let recetaInversa = [...receta].reverse();
+
+console.log(receta);
+console.log(recetaInversa);
